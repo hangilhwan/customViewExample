2c9b131d253db0a8428a41615084bcd5fe3d8589
gksrlfghks <jp.ghhan@gmail.com>
2020-11-01 00:31:37 +0900

    constraintlayout

---
+++ app/src/main/java/com/ghhan/customviewexample/ConstraintLayoutExampleFragment.kt
@@ -0,0 +1,27 @@
+package com.ghhan.customviewexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class ConstraintLayoutExampleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
